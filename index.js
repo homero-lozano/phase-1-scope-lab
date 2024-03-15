534f1f6fab9287044540e4738ec6eba60b333761
Nico Lozano <homeronicolaslozano@gmail.com>
2024-03-15 14:38:38 -0400

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob' 
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+        customerName = customerName.toUpperCase(); 
+     } else { 
+            throw new Error;
+        }
+    }
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+} 
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'anyone'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'larry' 
+} 
+changeLeastFavoriteCustomer();
+
+
+
+
